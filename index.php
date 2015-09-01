ec60dd42f601cf16cd5fb985aaa5f99a8d3ee0ed
Gergely Pap <gpap@brainsum.com>
2015-09-01 17:38:44 +0200

    lots of changes in less files, mainly header css

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="Brainsum International - Let's build your online business!">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <title>Brainsum International</title>
+
   <link rel="stylesheet" href="dist/css/app.min.css">
 </head>
 <body>
 
   <!-- HEADER -->
-  <header>
+  <header id="header" class="clearfix">
     <nav id="menu">
       <ul class="list-inline">
         <li><a href="#top">Home</a></li>
@@ -18,13 +23,37 @@
       </ul>
     </nav>
 
-    <div id="logo-wrapper">
+    <div id="header-left">
 
+      <div class="wrapper">
+        <object id="logo" data="/dist/img/logo.svg" type="image/svg+xml" width="230" height="150">
+          <img src="/dist/img/logo.svg" alt="BRAINSUM" width="230" height="150"/>
+        </object>
+        <span class="sub-logo spacious">International</span>
+      </div>
+
+      <div class="wrapper right">
+        <h1 class="title">Our <span class="hr">HR</span> software</h1>
+        <p class="info">
+          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+        </p>
+      </div>
+    </div>
+
+    <div id="featured" class="clearfix">
+      <img src="/dist/img/laborone-ref-img.jpg" alt="LaborOne HR Solutions" width="50%" height="50%">
+      <a class="spacious" href="http://www.laborone.ch">www.laborone.ch</a>
+    </div>
+
+    <div id="doing-more">
+      <h3>We are doing more</h3>
+      <img src="/dist/img/horisontal-laborone-mood-img.jpg" alt="LaborOne Solutions">
+      <h3>Lorem ipsum dolor sit amet.</h3>
     </div>
   </header>
 
   <!-- SECTIONS -->
 
-  <script src="dist/js/app.min.js" defer="defer" async="async"></script>
+  <script src="/dist/js/app.min.js" defer="defer" async="async"></script>
 </body>
 </html>
