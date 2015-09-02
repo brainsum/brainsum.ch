da5f2e13273e7eb4bc0fbf3cbe4388f4f4040a69
Gergely Pap <gpap@brainsum.com>
2015-09-02 13:53:46 +0200

    finished basic layout

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
           <span class="word">software</span>
         </h1>
         <p class="info">
-          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+          <span>State-of-the-art web design & development with Drupal.</span>
         </p>
       </div>
     </div>
@@ -57,7 +57,7 @@
     <div id="doing-more">
       <div class="wrapper">
         <h3>We are doing more</h3>
-        <h3>Lorem ipsum dolor sit amet.</h3>
+        <h3>Mobile & Web Development, E-commerce, Design</h3>
       </div>
     </div>
   </header>
@@ -71,10 +71,68 @@
   </section>
 
   <section id="portfolio">
-    <div class="container">
-      <h2 class="title">Our Work <span class="ampersand">&</span> Services</h2>
-      <p>Lorem</p>
+    <h2 class="section-title">Our Work <span class="ampersand">&</span> Services</h2>
+
+    <div class="contact-block">
+      <p><strong>Need a website</strong> for your company, brand or product?</p>
+      <button type="button" id="contact-button">Just drop us a message</button>
     </div>
+
+    <p>State-of-the-art web design & development with Drupal.</p>
+    <p>Mobile & Web Development / E-Commerce / WebDesign</p>
+
+    <div class="items">
+      <div class="item">
+        <a href="https://gravityrd.com">
+          <img src="/dist/img/gravityrd.jpg" alt="Gravity Research & Development">
+          <span class="name">Gravity Research & Development</span>
+        </a>
+      </div>
+      <div class="item">
+        <a href="http://molgroupcards.com">
+          <img src="/dist/img/molgroupcards.jpg" alt="MOL Group Cards">
+          <span class="name">MOL Group Cards</span>
+        </a>
+      </div>
+      <div class="item">
+        <a href="http://routing.palivovekarty.sk/en/">
+          <img src="/dist/img/stationfinder.jpg" alt="Slovnaft Station Finder">
+          <span class="name">Slovnaft Station Finder</span>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <section id="management">
+    <h2 class="section-title">Management</h2>
+    <div class="items container">
+      <div class="item">
+        <img src="" alt="">
+        <span class="name">Rinchen W. Chopathar</span>
+        <span>Managing Director</span>
+        <span>Management Board</span>
+        <a href="mailto:chopathar@brainsum.ch">chopathar@brainsum.ch</a>
+      </div>
+      <div class="item">
+        <img src="" alt="">
+        <span class="name">Peter Pónya</span>
+        <span>Managing Director</span>
+        <span>Management Board</span>
+        <a href="mailto:pedro@brainsum.ch">pedro@brainsum.ch</a>
+      </div>
+      <div class="item">
+        <img src="" alt="">
+        <span class="name">Balázs Kántor</span>
+        <span>Managing Director</span>
+        <span>Management Board</span>
+        <a href="mailto:bkantor@brainsum.ch">bkantor@brainsum.ch</a>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <a href="mailto:info@brainsum.ch">info@brainsum.ch</a>
+    <a href="tel:004112345564">+41 12354 5642</a>
   </section>
 
   <script src="/dist/js/app.min.js" defer="defer" async="async"></script>
