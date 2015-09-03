708fb08093ed7254978ff5f56545bd480bfd81a0
Gergely Pap <gpap@brainsum.com>
2015-09-03 11:21:40 +0200

    refactored app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,18 +1,34 @@
+/* globals window, document */
 (function (window, document) {
   "use strict";
 
-  window.onscroll = function() {
-    // holds the number of pixels from the top of the page
-    // that the viewport is currently at (as in, if you scroll
-    // down ten pixels from the top, this value is 10)
-    var offset = window.pageYOffset,
-        divider = 10,
-        back = document.getElementById('parallax-back');
+  /**
+   * Config object, for general settings.
+   * @type {Object}
+   */
+  var _ = {
+    /**
+     * The lower this is, the faster the background scrolls.
+     * @type {Number}
+     */
+    divider: 10,
 
-    // set the top margin of the cover image to the offset divided
-    // by some value. For example, below, for evert 1.5 pixels
-    // the main content moves, the image moves one
-    back.style.marginTop = (offset / divider) + "px";
+    /**
+     * Reference to the background element.
+     * @type {Node}
+     */
+    elem: document.getElementById('parallax-back'),
+
+    /**
+     * Initialize offset variable, which will be updated onscroll.
+     * @type {Float}
+     */
+    offset: null
+  };
+
+  window.onscroll = function() {
+    _.offset = window.pageYOffset;
+    _.elem.style.marginTop = (_.offset / _.divider) + "px";
   };
 
 })(window, document);
